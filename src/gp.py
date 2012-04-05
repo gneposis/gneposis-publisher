1e8f6633b4501992fc36c8da15bf120f61ef933b
Open Gnosis <gneposis@gmail.com>
2012-04-05 20:10:55 +0200

    working html export has been made

--- src/gp.py
+++ src/gp.py
@@ -13,157 +13,232 @@ parser = OptionParser()
 parser.add_option('-f', '--file', action='store', type='string', dest='filename')
 (options, args) = parser.parse_args()
 
-gp_dictionary = {}
-def get_gp_dictionary():
-    global gp_dictionary
+
+metas = ('author', 'title', 'subtitle')
+structs = ('part', 'chapter', 'intro')
+depth = {'part': 1, 'chapter': 2, 'intro': 'intro'}
+
+def dictionary():
+    dictionary = {}
     for dict_file in glob.glob('data/languages/*'):
         path_split = os.path.split(dict_file)
         lang = path_split[1]
-        gp_dictionary[lang] = {}
+        dictionary[lang] = {}
         line_number = 0
         with open(dict_file, encoding='utf-8') as a_file:
             for a_line in a_file:
                 line_number += 1
                 key = re.split('\W+?', a_line)[0]
                 value = re.split('\W+?', a_line)[2]
-                gp_dictionary[lang][key] = value
+                dictionary[lang][key] = value
+    return dictionary
 
-def get_language():
+def language():
     with open(options.filename, encoding='utf-8') as a_file:
         a_file.seek(0)
         first_line = a_file.readline()
         key = re.split('\W+?', first_line)[1]
-        for language in gp_dictionary:
-            if gp_dictionary[language]['author'] == key or gp_dictionary[language]['title'] == key:
+        for language in dictionary():
+            if dictionary()[language]['author'] == key or dictionary()[language]['title'] == key:
                 return language
 
-rawdata = None
-def parse_input_file():
-    global rawdata
+def raw():
     with open(options.filename, encoding='utf-8') as a_file:
-        rawdata = a_file.read()
+        raw = a_file.read()
         # deal with preamble
-        rawdata = re.sub(r'}\n{', r'}\\\\\n{', rawdata)
+        raw = re.sub(r'}\n{', r'}\\\\\n{', raw)
 
         # \n\n to @@@ and \n to \s and back with @@@s
-        rawdata = re.sub(r'\n\n', r'@@@', rawdata)
-        rawdata = re.sub(r'\n', r' ', rawdata)
-        rawdata = re.sub(r'@@@', r'\n\n', rawdata)
+        raw = re.sub(r'\n\n', r'@@@', raw)
+        raw = re.sub(r'\n', r' ', raw)
+        raw = re.sub(r' $', r'', raw)
+        raw = re.sub(r'@@@', r'\n\n', raw)
         
         # deal with linebreaks
-        rawdata = re.sub(r'\\\\\s{0,}', r'\\\\\n', rawdata)
+        raw = re.sub(r'\\\\\s{0,}', r'\\\\\n', raw)
         
         # deal with preamble again
-        rawdata = re.sub(r'}\\\\\n{', r'}\n{', rawdata)
+        raw = re.sub(r'}\\\\\n{', r'}\n{', raw)
+    return raw
 
-def utfize(gp_input):
-    output = gp_input
-    output = re.sub('--', '–', output)
-    output = re.sub(',,', '„', output)
-    output = re.sub("''", "”", output)
-    return output
+def utfize(gp):
+    gp = gp.replace('--','–')
+    gp = gp.replace(',,','„')
+    gp = gp.replace("''","”")
+    return gp
 
-def get_meta():
-    def get_author():
-        p = re.compile(r'{'+gp_dictionary[language]['author']+' (?P<author>.+?)}')
-        m = p.search(source_data)
-        return m.group('author')
-    def get_subtitle():
-        p = re.compile(r'{'+gp_dictionary[language]['subtitle']+' (?P<subtitle>.+?)}')
-        m = p.search(source_data)
-        return m.group('subtitle')
-    def get_title():
-        p = re.compile(r'{'+gp_dictionary[language]['title']+' (?P<title>.+?)}')
-        m = p.search(source_data)
-        return m.group('title')
-    return {'author': get_author(),
-            'subtitle': get_subtitle(),
-            'title': get_title()}
+def meta(gp):
+    meta = {}
+    for m in metas:
+        p = re.compile(r'{'+dictionary()[language()][m]+' (?P<'+m+'>.+?)}')
+        s = p.search(gp)
+        meta[m] = s.group(m)
+    return meta
 
-def get_parts():
-    parts = []
-    searching = True
-    partindex = 1
-    offset = 0
-    while searching:
-        p = re.compile('{'+gp_dictionary[language]['part']+' (?P<part>.+?)}')
-        m = p.search(source_data[offset:])
-        if m:
-            p2 = re.compile(r'(?P<parttitle>.+?)\s::\s(?P<partsubtitle>.+?)$')
-            m1 = p2.search(m.group('part'))
-            parts.append((offset+m.start(), offset+m.end(),'PART', m1.group('parttitle'), m1.group('partsubtitle')))
-            partindex += 1
-            offset += m.end()
-        else: 
-            searching = False
-    return parts
-
-def get_chapters():
-    chapters = []
-    searching = True
-    chapterindex = 1
-    offset = 0
-    while searching:
-        p = re.compile('{'+gp_dictionary[language]['chapter']+' (?P<chapter>.+?)}')
-        m = p.search(source_data[offset:])
-        if m:
-            p2 = re.compile(r'(?P<chaptertitle>.+?)\s::\s(?P<chaptersubtitle>.+?)$')
-            m1 = p2.search(m.group('chapter'))
-            chapters.append((offset+m.start(), offset+m.end(),'CHAPTER', m1.group('chaptertitle'), m1.group('chaptersubtitle')))
-            chapterindex += 1
-            offset += m.end()
-        else: 
-            searching = False
-    return chapters
-
-def get_struct_anchors():
-    struct_anchors = []
-    parts = get_parts()
-    i = 0
-    while i < len(parts):
-        struct_anchors.append(parts[i][0])
-        i +=1
-    chapters = get_chapters()
-    i = 0
-    while i < len(chapters):
-        struct_anchors.append(chapters[i][0])
-        i +=1
-    struct_anchors.sort()
-    return struct_anchors
-
-def get_struct():
+def struct(gp):
     struct = []
-    tempstruct = get_parts()
-    tempstruct.extend(get_chapters())
-    for i in range(len(get_struct_anchors())):
-        for j in range(len(tempstruct)):
-            if get_struct_anchors()[i] == tempstruct[j][0]:
-                struct.append(tempstruct[j])
-    return struct
-    
+    for s in structs:
+        searching = True
+        i = 1
+        o = 0
+        while searching:
+            p1 = re.compile('{'+dictionary()[language()][s]+' (?P<'+s+'>.+?)}')
+            m1 = p1.search(gp[o:])
+            if m1:
+                p2 = re.compile(r'(?P<'+s+'title>.+?)\s::\s(?P<'+s+'subtitle>.+?)$')
+                m2 = p2.search(m1.group(s))
+                if m2:
+                    struct.append((o+m1.start(), o+m1.end(),(depth[s], i), s, m2.group(s+'title'), m2.group(s+'subtitle')))
+                else:
+                    struct.append((o+m1.start(), o+m1.end(),(depth[s], i), s, m1.group(s), None))
+                i += 1
+                o += m1.end()
+            else:
+                searching = False
+    return sorted(struct)
 
-def htmlize():
-    with open('output/body.html', 'a', encoding='utf-8') as a_body:
-        with open('data/kindle/default/html_body_header', encoding='utf-8') as a_header:
+def footnotes(gp):
+    footnotes = []
+    searching = True
+    i = 1
+    o = 0
+    while searching:
+        p = re.compile('{'+dictionary()[language()]['footnote']+' (?P<footnote>.+?)}')
+        m = p.search(gp[o:])
+        if m:
+            footnotes.append((o+m.start(), o+m.end(), i, m.group('footnote')))
+            i += 1
+            o += m.end()
+        else: 
+            searching = False
+    return footnotes
+
+def get_re_string(input_string):
+    string = input_string
+    string = string.replace('\\','\\\\')
+    string = string.replace('.','\.')
+    string = string.replace('^','\^')
+    string = string.replace('$','\$')
+    string = string.replace('*','\*')
+    string = string.replace('+','\+')
+    string = string.replace('?','\?')
+    string = string.replace('{','\{')
+    string = string.replace('}','\}')
+    string = string.replace('[','\[')
+    string = string.replace(']','\]')
+    string = string.replace('|','\|')
+    string = string.replace('(','\(')
+    string = string.replace(')','\)')
+    return string
+
+def kindelize(gp):
+    def htmlize(gp):
+        saved_footnotes = []
+        def remove_meta(gp):
+            print('Remove metadata... ', end='')
+            r = gp
+            for m in metas:
+                p = re.compile(r'{'+dictionary()[language()][m]+' (.+?)}\n')
+                r = re.sub(p, r'', r)
+            print('DONE')
+            return r
+        def add_header(gp):
+            print('Generating header... ', end='')
+            header = open('data/kindle/default/html_body_header', encoding='utf-8')
             p = re.compile(r'AUTHOR: TITLE')
-            new_header = p.sub(meta['author']+': '+meta['title'],a_header.read())
-        a_body.write(new_header)
-
-#def htmlize(gp_input):
-#    def conv_emph(htmlize_input):
-#        pattern = re.compile(r'{'+gp_dictionary[language]['emph']+' (.+?)}')
-#        output = re.sub(pattern, r'<i>\1</i>', htmlize_input)
-#        return output
-#    output = conv_emph(gp_input)
-#    return output
-        
+            r = p.sub(meta(utfize(raw()))['author']+': '+meta(utfize(raw()))['title'],header.read())
+            header.close()
+            print('DONE')
+            return r + gp
+        def conv_br(gp):
+            print('Convert linebreaks... ', end='')
+            p = re.compile(r'\s{0,}?\\\\')
+            r = re.sub(p, r'<br/>', gp)
+            print('DONE')
+            return r
+        def conv_emph(gp):
+            print('Convert emphasized parts... ', end='')
+            p = re.compile(r'{'+dictionary()[language()]['emph']+' (.+?)}')
+            r = re.sub(p, r'<i>\1</i>', gp)
+            print('DONE')
+            return r
+        def conv_p(gp):
+            print('Convert paragraphs... ', end='')
+            p1 = re.compile(r'\n\n(?!{)')
+            r1 = re.sub(p1, r'\n<p>', gp)
+            p2 = re.compile(r'^<p>(.+?)$', re.M)
+            r2 = re.sub(p2, r'<p>\1</p>', r1)
+            print('DONE')
+            return r2
+        def conv_paragraph(gp):
+            print('Convert paragraph titles... ', end='')
+            p = re.compile(r'{'+dictionary()[language()]['paragraph']+' (.+?)}')
+            r = re.sub(p, r'<p class="paragraph">\1</p>', gp)
+            print('DONE')
+            return r
+        def conv_right(gp):
+            print('Convert right indented blocks... ', end='')
+            p = re.compile(r'{'+dictionary()[language()]['right']+' (.+?)}')
+            r = re.sub(p, r'<div align="right">\1</div>', gp)
+            print('DONE')
+            return r
+        def conv_footnotes(gp):
+            nonlocal saved_footnotes
+            saved_footnotes = footnotes(gp)
+            print('Convert inline footnotes... ', end='')
+            r = gp
+            for f in range(len(footnotes(gp))):
+                p = re.compile('{'+dictionary()[language()]['footnote']+' '+get_re_string(footnotes(gp)[f][3])+'}')
+                r = p.sub('<a name="fn_name_'+str(footnotes(gp)[f][2])+'" href="#fn_index_'+str(footnotes(gp)[f][2])+'"><span class="footnote"><sup>'+str(footnotes(gp)[f][2])+'</sup></span></a>',r)
+            print('DONE')
+            return r
+        def add_footnotes(gp):
+            print('Generating footnotes... ', end='')
+            gp = gp + '\n\n<a name="footnotes"><div class="footnotes">'+dictionary()[language()]['notes']+':</div></a>\n'
+            for f in range(len(saved_footnotes)):
+                footnote = '<div class="footnoteitem"><a name="fn_index_'+str(saved_footnotes[f][2])+'" href="#fn_name_'+str(saved_footnotes[f][2])+'">['+str(saved_footnotes[f][2])+']</a>'+saved_footnotes[f][3]+'</div>\n'
+                gp = gp + footnote
+            print('DONE')
+            return gp
+        def add_footer(gp):
+            print('Generating footer... ', end='')
+            gp = gp + '\n</body>\n</html>'
+            print('DONE')
+            return gp
+        def conv_struct(gp):
+            print('Convert text parts... ', end='')
+            r = gp
+            for s in range(len(struct(gp))):
+                if struct(gp)[s][5] is None:
+                    p = re.compile('{'+dictionary()[language()][struct(gp)[s][3]]+' '+get_re_string(struct(gp)[s][4])+'}')
+                    r = p.sub('<a name="'+str(struct(gp)[s][2][0])+'-'+str(struct(gp)[s][2][1])+'"><div class="'+struct(gp)[s][3]+'_alone">'+struct(gp)[s][4]+'</div></a>',r)
+                else:
+                    p = re.compile('{'+dictionary()[language()][struct(gp)[s][3]]+' '+get_re_string(struct(gp)[s][4])+' :: '+get_re_string(struct(gp)[s][5])+'}')
+                    r = p.sub('<a name="'+str(struct(gp)[s][2][0])+'-'+str(struct(gp)[s][2][1])+'"><div class="'+struct(gp)[s][3]+'_withtitle">'+struct(gp)[s][4]+'</div></a>\n<div class="'+struct(gp)[s][3]+'title">'+struct(gp)[s][5]+'</div>',r)
+            print('DONE')
+            return r
+        def conv_verse(gp):
+            print('Convert verse... ', end='')
+            p = re.compile(r'{'+dictionary()[language()]['verse']+' (.+?)}', re.S)
+            r = re.sub(p, r'<div class="verse">\1</div>', gp)
+            print('DONE')
+            return r
+        gp = remove_meta(gp)
+        gp = add_header(gp)
+        gp = conv_emph(gp)
+        gp = conv_right(gp)
+        gp = conv_p(gp)
+        gp = conv_paragraph(gp)
+        gp = conv_footnotes(gp)
+        gp = add_footnotes(gp)
+        gp = conv_struct(gp)
+        gp = conv_verse(gp)
+        gp = conv_br(gp)
+        gp = add_footer(gp)
+        return gp
+    return htmlize(gp)
 
 if __name__ == '__main__':
 #    pass
-    get_gp_dictionary()
-    language = get_language()
-    parse_input_file()
-    source_data = utfize(rawdata)
-    meta = get_meta()
-    struct = get_struct()
-
+    with open('output/body.html', 'w', encoding='utf-8') as a_body:
+        a_body.write(kindelize(utfize(raw())))
