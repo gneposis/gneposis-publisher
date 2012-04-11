44159939f901d766547e44bd69829a95341cfffb
Open Gnosis <gneposis@gmail.com>
2012-04-11 20:38:41 +0200

    fixes+additions+toc.ncx creation

--- src/gp.py
+++ src/gp.py
@@ -14,9 +14,23 @@ parser.add_option('-f', '--file', action='store', type='string', dest='filename'
 (options, args) = parser.parse_args()
 
 
-metas = ('author', 'title', 'subtitle')
-structs = ('part', 'chapter', 'intro')
-depth = {'part': 1, 'chapter': 2, 'intro': 'intro'}
+metas = ('author', 'title', 'subtitle', 'copyright')
+structs = ('part', 'chapter', 'intro', 'foreword')
+depth = {'part': 1, 'chapter': 2, 'intro': 0, 'foreword': 'foreword'}
+
+
+# Code:  http://stackoverflow.com/a/7829658
+import hashlib
+from functools import partial
+
+def md5sum(filename):
+    with open(filename, mode='rb') as f:
+        d = hashlib.md5()
+        for buf in iter(partial(f.read, 128), b''):
+            d.update(buf)
+    return d.hexdigest()
+
+####
 
 def dictionary():
     dictionary = {}
@@ -70,7 +84,7 @@ def utfize(gp):
 def meta(gp):
     meta = {}
     for m in metas:
-        p = re.compile(r'{'+dictionary()[language()][m]+' (?P<'+m+'>.+?)}')
+        p = re.compile(r'{'+dictionary()[language()][m]+' (?P<'+m+'>.+?)}', re.S)
         s = p.search(gp)
         meta[m] = s.group(m)
     return meta
@@ -131,114 +145,229 @@ def get_re_string(input_string):
     string = string.replace(')','\)')
     return string
 
-def kindelize(gp):
-    def htmlize(gp):
-        saved_footnotes = []
-        def remove_meta(gp):
-            print('Remove metadata... ', end='')
-            r = gp
-            for m in metas:
-                p = re.compile(r'{'+dictionary()[language()][m]+' (.+?)}\n')
-                r = re.sub(p, r'', r)
-            print('DONE')
-            return r
-        def add_header(gp):
-            print('Generating header... ', end='')
-            header = open('data/kindle/default/html_body_header', encoding='utf-8')
-            p = re.compile(r'AUTHOR: TITLE')
-            r = p.sub(meta(utfize(raw()))['author']+': '+meta(utfize(raw()))['title'],header.read())
-            header.close()
-            print('DONE')
-            return r + gp
-        def conv_br(gp):
-            print('Convert linebreaks... ', end='')
-            p = re.compile(r'\s{0,}?\\\\')
-            r = re.sub(p, r'<br/>', gp)
-            print('DONE')
-            return r
-        def conv_emph(gp):
-            print('Convert emphasized parts... ', end='')
-            p = re.compile(r'{'+dictionary()[language()]['emph']+' (.+?)}')
-            r = re.sub(p, r'<i>\1</i>', gp)
-            print('DONE')
-            return r
-        def conv_p(gp):
-            print('Convert paragraphs... ', end='')
-            p1 = re.compile(r'\n\n(?!{)')
-            r1 = re.sub(p1, r'\n<p>', gp)
-            p2 = re.compile(r'^<p>(.+?)$', re.M)
-            r2 = re.sub(p2, r'<p>\1</p>', r1)
-            print('DONE')
-            return r2
-        def conv_paragraph(gp):
-            print('Convert paragraph titles... ', end='')
-            p = re.compile(r'{'+dictionary()[language()]['paragraph']+' (.+?)}')
-            r = re.sub(p, r'<p class="paragraph">\1</p>', gp)
-            print('DONE')
-            return r
-        def conv_right(gp):
-            print('Convert right indented blocks... ', end='')
-            p = re.compile(r'{'+dictionary()[language()]['right']+' (.+?)}')
-            r = re.sub(p, r'<div align="right">\1</div>', gp)
-            print('DONE')
-            return r
-        def conv_footnotes(gp):
-            nonlocal saved_footnotes
-            saved_footnotes = footnotes(gp)
-            print('Convert inline footnotes... ', end='')
-            r = gp
-            for f in range(len(footnotes(gp))):
-                p = re.compile('{'+dictionary()[language()]['footnote']+' '+get_re_string(footnotes(gp)[f][3])+'}')
-                r = p.sub('<a name="fn_name_'+str(footnotes(gp)[f][2])+'" href="#fn_index_'+str(footnotes(gp)[f][2])+'"><span class="footnote"><sup>'+str(footnotes(gp)[f][2])+'</sup></span></a>',r)
-            print('DONE')
-            return r
-        def add_footnotes(gp):
-            print('Generating footnotes... ', end='')
-            gp = gp + '\n\n<a name="footnotes"><div class="footnotes">'+dictionary()[language()]['notes']+':</div></a>\n'
-            for f in range(len(saved_footnotes)):
-                footnote = '<div class="footnoteitem"><a name="fn_index_'+str(saved_footnotes[f][2])+'" href="#fn_name_'+str(saved_footnotes[f][2])+'">['+str(saved_footnotes[f][2])+']</a>'+saved_footnotes[f][3]+'</div>\n'
-                gp = gp + footnote
-            print('DONE')
-            return gp
-        def add_footer(gp):
-            print('Generating footer... ', end='')
-            gp = gp + '\n</body>\n</html>'
-            print('DONE')
-            return gp
-        def conv_struct(gp):
-            print('Convert text parts... ', end='')
-            r = gp
-            for s in range(len(struct(gp))):
-                if struct(gp)[s][5] is None:
-                    p = re.compile('{'+dictionary()[language()][struct(gp)[s][3]]+' '+get_re_string(struct(gp)[s][4])+'}')
-                    r = p.sub('<a name="'+str(struct(gp)[s][2][0])+'-'+str(struct(gp)[s][2][1])+'"><div class="'+struct(gp)[s][3]+'_alone">'+struct(gp)[s][4]+'</div></a>',r)
-                else:
-                    p = re.compile('{'+dictionary()[language()][struct(gp)[s][3]]+' '+get_re_string(struct(gp)[s][4])+' :: '+get_re_string(struct(gp)[s][5])+'}')
-                    r = p.sub('<a name="'+str(struct(gp)[s][2][0])+'-'+str(struct(gp)[s][2][1])+'"><div class="'+struct(gp)[s][3]+'_withtitle">'+struct(gp)[s][4]+'</div></a>\n<div class="'+struct(gp)[s][3]+'title">'+struct(gp)[s][5]+'</div>',r)
-            print('DONE')
-            return r
-        def conv_verse(gp):
-            print('Convert verse... ', end='')
-            p = re.compile(r'{'+dictionary()[language()]['verse']+' (.+?)}', re.S)
-            r = re.sub(p, r'<div class="verse">\1</div>', gp)
-            print('DONE')
-            return r
-        gp = remove_meta(gp)
-        gp = add_header(gp)
-        gp = conv_emph(gp)
-        gp = conv_right(gp)
-        gp = conv_p(gp)
-        gp = conv_paragraph(gp)
-        gp = conv_footnotes(gp)
-        gp = add_footnotes(gp)
-        gp = conv_struct(gp)
-        gp = conv_verse(gp)
-        gp = conv_br(gp)
-        gp = add_footer(gp)
+def tocname(tocelement):
+    tocname = None
+    try:
+        check = tocelement[2][0] >= 0
+    except:
+        check = False
+    if check:
+        if tocelement[2][0] < 2:
+            if tocelement[5]:
+                tocname = tocelement[4]+': '+tocelement[5]
+            else:
+                tocname = tocelement[4]
+        else:
+            if tocelement[5]:
+                tocname = str(tocelement[2][1])+'. '+tocelement[5]
+            else:
+                tocname = str(tocelement[2][1])+'. '+tocelement[4]
+    return tocname
+
+saved_footnotes_kindle_html = []
+
+def kindle_html(gp):
+    def remove_meta(gp):
+        r = gp
+        for m in metas:
+            p = re.compile(r'{'+get_re_string(dictionary()[language()][m])+' (.+?)}\n', re.S)
+            r = re.sub(p, r'', r)
+        print('DONE')
+        return r
+    def add_header(gp):
+        header = open('data/kindle/default/html_body_header', encoding='utf-8')
+        p = re.compile(r'AUTHOR: TITLE')
+        r = p.sub(mainmeta['author']+': '+mainmeta['title'],header.read())
+        header.close()
+        print('DONE')
+        return r + gp
+    def add_toc(gp):
+        n = len(mainstruct)
+        header = '<a name="toc"><div class="toc">'+dictionary()[language()]['Toc']+'</div></a>\n'
+        body = ''
+        for s in range(n):
+            try:
+                check = mainstruct[n-s-1][2][0] >= 0
+            except:
+                check = False
+            if check:
+                body = '<div class="toclvl'+str(mainstruct[n-s-1][2][0])+'"><a href="#'+str(mainstruct[n-s-1][2][0])+'-'+str(mainstruct[n-s-1][2][1])+'">'+tocname(mainstruct[n-s-1])+'</a></div>\n'+body
+        print('DONE')
+        return header + body + gp
+    def add_titlepage(gp):
+        page = '<a name="start"/>\n<div class="author">'+mainmeta['author']+'</div>\n'+'<div class="title">'+mainmeta['title']+'</div>\n'+'<div class="subtitle">'+mainmeta['subtitle']+'</div>\n'+'<div style="page-break-after:always; margin-top: 2%"></div>\n'
+        print('DONE')
+        return page + gp
+    def add_copyrightpage(gp):
+        page = '<div class="copyright">'+mainmeta['copyright']+'</div>\n'
+        print('DONE')
+        return page + gp
+    def conv_br(gp):
+        p = re.compile(r'\s{0,}?\\\\')
+        r = re.sub(p, r'<br/>', gp)
+        print('DONE')
+        return r
+    def conv_emph(gp):
+        p = re.compile(r'{'+dictionary()[language()]['emph']+' (.+?)}')
+        r = re.sub(p, r'<i>\1</i>', gp)
+        print('DONE')
+        return r
+    def conv_p(gp):
+        p1 = re.compile(r'\n\n(?!{)')
+        r1 = re.sub(p1, r'\n<p>', gp)
+        p2 = re.compile(r'^<p>(.+?)$', re.M)
+        r2 = re.sub(p2, r'<p>\1</p>', r1)
+        print('DONE')
+        return r2
+    def conv_paragraph(gp):
+        p = re.compile(r'{'+dictionary()[language()]['paragraph']+' (.+?)}')
+        r = re.sub(p, r'<p class="paragraph">\1</p>', gp)
+        print('DONE')
+        return r
+    def conv_right(gp):
+        p = re.compile(r'{'+dictionary()[language()]['right']+' (.+?)}')
+        r = re.sub(p, r'<div align="right">\1</div>', gp)
+        print('DONE')
+        return r
+    def conv_footnotes(gp):
+        global saved_footnotes_kindle_html
+        saved_footnotes_kindle_html = footnotes(gp)
+        r = gp
+        for f in range(len(footnotes(gp))):
+            p = re.compile('{'+dictionary()[language()]['footnote']+' '+get_re_string(footnotes(gp)[f][3])+'}')
+            r = p.sub('<a name="fn_name_'+str(footnotes(gp)[f][2])+'" href="#fn_index_'+str(footnotes(gp)[f][2])+'"><span class="footnote"><sup>'+str(footnotes(gp)[f][2])+'</sup></span></a>',r)
+        print('DONE')
+        return r
+    def add_footnotes(gp):
+        gp = gp + '\n\n<a name="footnotes"><div class="footnotes">'+dictionary()[language()]['Notes']+':</div></a>\n'
+        for f in range(len(saved_footnotes_kindle_html)):
+            footnote = '<div class="footnoteitem"><a name="fn_index_'+str(saved_footnotes_kindle_html[f][2])+'" href="#fn_name_'+str(saved_footnotes_kindle_html[f][2])+'">['+str(saved_footnotes_kindle_html[f][2])+']</a> '+saved_footnotes_kindle_html[f][3]+'</div>\n'
+            gp = gp + footnote
+        print('DONE')
         return gp
-    return htmlize(gp)
+    def add_footer(gp):
+        gp = gp + '\n</body>\n</html>'
+        print('DONE')
+        return gp
+    def conv_struct(gp):
+        r = gp
+        for s in range(len(mainstruct)):
+            if mainstruct[s][5] is None:
+                p = re.compile('{'+dictionary()[language()][mainstruct[s][3]]+' '+get_re_string(mainstruct[s][4])+'}')
+                r = p.sub('<a name="'+str(mainstruct[s][2][0])+'-'+str(mainstruct[s][2][1])+'"><div class="'+mainstruct[s][3]+'_alone">'+mainstruct[s][4]+'</div></a>',r)
+            else:
+                p = re.compile('{'+dictionary()[language()][mainstruct[s][3]]+' '+get_re_string(mainstruct[s][4])+' :: '+get_re_string(mainstruct[s][5])+'}')
+                r = p.sub('<a name="'+str(mainstruct[s][2][0])+'-'+str(mainstruct[s][2][1])+'"><div class="'+mainstruct[s][3]+'_withtitle">'+mainstruct[s][4]+'</div></a>\n<div class="'+mainstruct[s][3]+'title">'+mainstruct[s][5]+'</div>',r)
+        print('DONE')
+        return r
+    def conv_announcement(gp):
+        p1 = re.compile(r'{'+dictionary()[language()]['announcement']+' (.+?)}')
+        r1 = re.sub(p1, r'<div class="announcementtitle">\1</div>', gp)
+        p2 = re.compile(r'{'+dictionary()[language()]['announcement']+' (.+?)}')
+        r2 = re.sub(p2, r'<div class="announcementtitle">\1</div>', r1)
+        print('DONE')
+        return r2
+    def conv_verse(gp):
+        p = re.compile(r'{'+dictionary()[language()]['verse']+' (.+?)}', re.S)
+        r = re.sub(p, r'<div class="verse">\1</div>', gp)
+        print('DONE')
+        return r
+    print('\nGenerating Kindle compatible HTML file:')
+    print('-'*80)
+    print('    Remove metadata... ', end='')
+    gp = remove_meta(gp)
+    print('    Convert emphasized parts... ', end='')
+    gp = conv_emph(gp)
+    print('    Convert paragraphs... ', end='')
+    gp = conv_p(gp)
+    print('    Generating TOC... ', end='')
+    gp = add_toc(gp)
+    print('    Add copyright page... ', end='')
+    gp = add_copyrightpage(gp)
+    print('    Add titlepage... ', end='')
+    gp = add_titlepage(gp)
+    print('    Generating header... ', end='')
+    gp = add_header(gp)
+    print('    Convert right indented blocks... ', end='')
+    gp = conv_right(gp)
+    print('    Convert paragraph titles... ', end='')
+    gp = conv_paragraph(gp)
+    print('    Convert inline footnotes... ', end='')
+    gp = conv_footnotes(gp)
+    print('    Generating footnotes... ', end='')
+    gp = add_footnotes(gp)
+    print('    Convert text parts... ', end='')
+    gp = conv_struct(gp)
+    print('    Convert announcements... ', end='')
+    gp = conv_announcement(gp)
+    print('    Convert verse... ', end='')
+    gp = conv_verse(gp)
+    print('    Convert linebreaks... ', end='')
+    gp = conv_br(gp)
+    print('    Generating footer... ', end='')
+    gp = add_footer(gp)
+    return gp
+
+def kindle_ncx():
+    def add_header():
+        h = open('data/kindle/default/ncx_header', encoding='utf-8')
+        header = h.read()
+        h.close()
+        print('DONE')
+        return header
+    def add_head():
+        head = '<head>\n<meta name="dtb:uid" content="'+mainmeta['hash']+'"/> <!-- same as in .opf -->\n<meta name="dtb:depth" content="1"/> <!-- 1 or higher -->\n<meta name="dtb:totalPageCount" content="0"/> <!-- must be 0 -->\n<meta name="dtb:maxPageNumber" content="0"/> <!-- must be 0 -->\n</head>'
+        print('DONE')
+        return head
+    def add_doctitle_docauthor():
+        content = '\n<docTitle>\n<text>'+mainmeta['title']+'</text>\n</docTitle>'+'\n<docAuthor>\n<text>'+mainmeta['author']+'</text>\n</docAuthor>'
+        print('DONE')
+        return content
+    def add_navmap():
+        def add_navpoint():
+            content = ''
+            n = len(mainstruct)
+            i = 1
+            for s in range(n):
+                try:
+                    check = mainstruct[s][2][0] >= 0
+                except:
+                    check = False
+                if check:
+                    nav_id = str(mainstruct[s][2][0])+'-'+str(mainstruct[s][2][1])
+                    content = content+'\n<navPoint class="'+mainstruct[s][3]+'" id="'+nav_id+'" playOrder="'+str(i)+'">\n<navLabel><text>'+tocname(mainstruct[s])+'</text></navLabel>\n<content src=body.html#'+nav_id+' />\n</navPoint>'
+                    i += 1      
+            return content
+        content = '\n<navMap>'+add_navpoint()+'\n</navMap>\n</ncx>'
+        print('DONE')                                                                                
+        return content
+    print('\nGenerating Kindle compatible TOC.ncx file:')
+    print('-'*80)
+    print('    Generating header... ', end='')
+    ncx = add_header()
+    print('    Generating <head>... ', end='')
+    ncx = ncx+add_head()
+    print('    Generating <docTitle>,<docAuthor>... ', end='')
+    ncx = ncx+add_doctitle_docauthor()
+    print('    Generating <navMap>... ', end='')
+    ncx = ncx+add_navmap()
+    return ncx
 
 if __name__ == '__main__':
 #    pass
+    print('gneposis-publisher 0.1alpha by Adam Szieberth')
+    print('='*80)
+    
+    
+    data = utfize(raw())
+    mainmeta = meta(data)
+    mainmeta['hash'] = md5sum(options.filename)
+    mainstruct = struct(data)
+    mainfootnotes = footnotes(data)
+
     with open('output/body.html', 'w', encoding='utf-8') as a_body:
-        a_body.write(kindelize(utfize(raw())))
+        a_body.write(kindle_html(data))
+
+    with open('output/toc.ncx', 'w', encoding='utf-8') as a_ncx:
+        a_ncx.write(kindle_ncx())
+    
