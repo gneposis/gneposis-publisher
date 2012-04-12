c6657e799ef9719bbe034a916674745a1d0b8c5e
Open Gnosis <gneposis@gmail.com>
2012-04-12 13:37:11 +0200

    opf file creation,first working version with reference input file

--- src/gp.py
+++ src/gp.py
@@ -7,14 +7,17 @@ Created on Apr 2, 2012
 import os
 import glob
 import re
+import unicodedata
+import sys
 from optparse import OptionParser
+from shutil import copy
 
 parser = OptionParser()
 parser.add_option('-f', '--file', action='store', type='string', dest='filename')
 (options, args) = parser.parse_args()
 
 
-metas = ('author', 'title', 'subtitle', 'copyright')
+metas = ('title', 'subtitle', 'copyright', 'date', 'description', 'isbn')
 structs = ('part', 'chapter', 'intro', 'foreword')
 depth = {'part': 1, 'chapter': 2, 'intro': 0, 'foreword': 'foreword'}
 
@@ -82,11 +85,29 @@ def utfize(gp):
     return gp
 
 def meta(gp):
+    def get_language():
+        if language() == 'default':
+            lang='en'
+        else:
+            lang=language()
+        return lang
+    def get_author():
+        p1 = re.compile(r'{'+dictionary()[language()]['author']+' (?P<c>.+?)}', re.S)
+        m1 = p1.search(gp)
+        d = {}
+        if m1:
+            p2 = re.compile(r'(?P<author>.+?)\s::\s(?P<sortauthor>.+?)$')
+            m2 = p2.search(m1.group('c'))
+            d['author']=m2.group('author')
+            d['sortauthor']=m2.group('sortauthor')
+        return d
     meta = {}
     for m in metas:
         p = re.compile(r'{'+dictionary()[language()][m]+' (?P<'+m+'>.+?)}', re.S)
         s = p.search(gp)
         meta[m] = s.group(m)
+    meta=dict(list(meta.items()) + list(get_author().items()))
+    meta['language']=get_language()
     return meta
 
 def struct(gp):
@@ -145,6 +166,14 @@ def get_re_string(input_string):
     string = string.replace(')','\)')
     return string
 
+# Code:  http://code.activestate.com/recipes/576648-remove-diatrical-marks-including-accents-from-stri/
+def remove_diacritic(i):
+    '''
+    Accept a unicode string, and return a normal string (bytes in Python 3)
+    without any diacritical marks.
+    '''
+    return unicodedata.normalize('NFKD', i).encode('ASCII', 'ignore').decode()
+
 def tocname(tocelement):
     tocname = None
     try:
@@ -172,13 +201,15 @@ def kindle_html(gp):
         for m in metas:
             p = re.compile(r'{'+get_re_string(dictionary()[language()][m])+' (.+?)}\n', re.S)
             r = re.sub(p, r'', r)
+        p2 = re.compile(r'{'+dictionary()[language()]['author']+' (.+?)}\n', re.S)
+        r2 = re.sub(p2, r'', r)
         print('DONE')
-        return r
+        return r2
     def add_header(gp):
-        header = open('data/kindle/default/html_body_header', encoding='utf-8')
+        with open('data/kindle/default/html_body_header', 'r', encoding='utf-8') as a_header:
+            header = a_header.read()+'\n'
         p = re.compile(r'AUTHOR: TITLE')
-        r = p.sub(mainmeta['author']+': '+mainmeta['title'],header.read())
-        header.close()
+        r = p.sub(mainmeta['author']+': '+mainmeta['title'],header)
         print('DONE')
         return r + gp
     def add_toc(gp):
@@ -241,7 +272,7 @@ def kindle_html(gp):
     def add_footnotes(gp):
         gp = gp + '\n\n<a name="footnotes"><div class="footnotes">'+dictionary()[language()]['Notes']+':</div></a>\n'
         for f in range(len(saved_footnotes_kindle_html)):
-            footnote = '<div class="footnoteitem"><a name="fn_index_'+str(saved_footnotes_kindle_html[f][2])+'" href="#fn_name_'+str(saved_footnotes_kindle_html[f][2])+'">['+str(saved_footnotes_kindle_html[f][2])+']</a> '+saved_footnotes_kindle_html[f][3]+'</div>\n'
+            footnote = '<div class="footnoteitem">[<a name="fn_index_'+str(saved_footnotes_kindle_html[f][2])+'" href="#fn_name_'+str(saved_footnotes_kindle_html[f][2])+'">'+str(saved_footnotes_kindle_html[f][2])+'</a>] '+saved_footnotes_kindle_html[f][3]+'</div>\n'
             gp = gp + footnote
         print('DONE')
         return gp
@@ -310,11 +341,12 @@ def kindle_html(gp):
 
 def kindle_ncx():
     def add_header():
-        h = open('data/kindle/default/ncx_header', encoding='utf-8')
-        header = h.read()
-        h.close()
+        with open('data/kindle/default/ncx_header', 'r', encoding='utf-8') as a_header:
+            header = a_header.read()+'\n'
+        p = re.compile(r'xml:lang="unknown"')
+        r = p.sub('xml:lang="'+mainmeta['language']+'"',header)
         print('DONE')
-        return header
+        return r
     def add_head():
         head = '<head>\n<meta name="dtb:uid" content="'+mainmeta['hash']+'"/> <!-- same as in .opf -->\n<meta name="dtb:depth" content="1"/> <!-- 1 or higher -->\n<meta name="dtb:totalPageCount" content="0"/> <!-- must be 0 -->\n<meta name="dtb:maxPageNumber" content="0"/> <!-- must be 0 -->\n</head>'
         print('DONE')
@@ -353,6 +385,96 @@ def kindle_ncx():
     ncx = ncx+add_navmap()
     return ncx
 
+def kindle_opf():
+    def add_header():
+        with open('data/kindle/default/opf_header', 'r', encoding='utf-8') as a_header:
+            header = a_header.read()
+        package = '<package unique-identifier="'+mainmeta['hash']+'">'
+        print('DONE')
+        return header+package+'\n'
+    def add_metadata():
+        def add_header():
+            with open('data/kindle/default/opf_metadata_header', 'r', encoding='utf-8') as a_header:
+                header = '<metadata>\n'+a_header.read()
+            return header
+        def add_common_metadata(meta):
+            content = '<dc:'+meta.capitalize()+'>'+mainmeta[meta]+'</dc:'+meta.capitalize()+'>'
+            return content+'\n'
+        def add_author():
+            content = '<dc:Creator opf:file-as="'+remove_diacritic(mainmeta['sortauthor'])+'" opf:role="aut">'+mainmeta['author']+'</dc:Creator>'
+            return content+'\n'
+        def add_identifier():
+            content = '<dc:Identifier id="uid">'+mainmeta['hash']+'</dc:Identifier>\n'
+            if mainmeta['isbn']:
+                content = content+'<dc:Identifier opf:scheme="ISBN">'+mainmeta['isbn']+'</dc:Identifier>\n'
+            return content
+        def add_footer():
+            content='''</dc-metadata>
+<x-metadata>
+<EmbeddedCover>cover.jpg</EmbeddedCover>
+<output encoding="utf-8"></output>
+</x-metadata>
+<meta name="cover" content="my-cover-image" />
+</metadata>
+'''
+            return content
+        metadata = add_header()
+        metadata = metadata + add_common_metadata('title')
+        metadata = metadata + add_common_metadata('language')
+        metadata = metadata + add_author()
+        metadata = metadata + add_common_metadata('date')
+        metadata = metadata + add_common_metadata('description')
+        metadata = metadata + add_identifier()
+        metadata = metadata + add_footer()
+        print('DONE')
+        return metadata
+    def add_manifest():
+        def add_cover():
+            content = '<item id="my-cover-image" media-type="image/jpeg" href="cover.jpg"/>\n'
+            return content
+        def add_tocncx():
+            content = '<item id="toc" media-type="application/x-dtbncx+xml" href="toc.ncx"/>\n'
+            return content
+        def add_html(htmlname):
+            content = '<item id="'+htmlname+'" media-type="text/x-oeb1-document" href="'+htmlname+'.html"/>\n'
+            return content
+        body = add_cover()+add_tocncx()+add_html('body')
+        print('DONE')
+        return '<manifest>\n'+body+'</manifest>\n'
+    def add_spine():
+        body = '<itemref idref="body"/>\n'
+        print('DONE')
+        return '<spine toc="toc">\n'+body+'</spine>\n'
+    def add_guide():
+        start = '<reference type="start" title="Go to Beginning" href="body.html#start"/>\n'
+        toc = '<reference type="toc" title="Table of Contents" href="body.html#toc"/>\n'
+        print('DONE')
+        return '<guide>\n'+start+toc+'</guide>\n'
+
+    print('\nGenerating Kindle compatible .opf file:')
+    print('-'*80)
+    print('    Generating header... ', end='')
+    opf = add_header()
+    print('    Generating metadata... ', end='')
+    opf = opf+add_metadata()
+    print('    Generating manifest... ', end='')
+    opf = opf+add_manifest()
+    print('    Generating spine... ', end='')
+    opf = opf+add_spine()
+    print('    Generating guide... ', end='')
+    opf = opf+add_guide()
+    return opf+'</package>'
+
+def kindle_copy():
+    print('\nCopying necessary files:')
+    print('-'*80)
+    print('    Copying .css file... ', end='')
+    copy('data/kindle/default/default.css','output')
+    print('DONE')
+    print('    Copying cover.jpg file... ', end='')
+    copy('cover.jpg','output')
+    print('DONE')
+
 if __name__ == '__main__':
 #    pass
     print('gneposis-publisher 0.1alpha by Adam Szieberth')
@@ -371,3 +493,7 @@ if __name__ == '__main__':
     with open('output/toc.ncx', 'w', encoding='utf-8') as a_ncx:
         a_ncx.write(kindle_ncx())
     
+    with open('output/body.opf', 'w', encoding='utf-8') as a_opf:
+        a_opf.write(kindle_opf())
+        
+    kindle_copy()
